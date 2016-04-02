92a0e340f270e6aef154ad757e93339c6c12dd9a
Omar-Real <realo865@gmail.com>
2016-04-02 20:40:34 +0300

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -11,7 +11,7 @@
 do
 
 function run(msg, matches)
-  return 'Real Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @Omar_Real\n https://github.com/Omar-Real/RealBot.git GNU GPL v2 license.'
+  return 'Real Bot V1 Supergroups\nAn advanced Administration bot based on The best of telegram \n\nDevelopers: @Omar_Real\n https://github.com/Omar-Real/RealBot.git GNU GPL v2 license.'
 end
 
 return {
